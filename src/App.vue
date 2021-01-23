fc5c8c6b0134511bf9a4e7a0a710c876499c7117
aditia kusuma <aditiakusuma800@gmail.com>
2021-01-23 12:36:57 +0700

    check auth

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,33 @@
 </template>
 
 <script lang="ts">
-import { IonApp, IonRouterOutlet } from '@ionic/vue';
-import { defineComponent } from 'vue';
+import { IonApp, IonRouterOutlet } from "@ionic/vue";
+import { defineComponent } from "vue";
+import { auth } from "@/firebase";
+import { useStore } from "vuex";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
     IonApp,
-    IonRouterOutlet
-  }
+    IonRouterOutlet,
+  },
+  setup() {
+    const store = useStore();
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        const currentUser = {
+          uid: user.uid,
+          displayName: user.displayName,
+          email: user.email,
+          photoURL: user.photoURL,
+          phoneNumber: user.phoneNumber,
+        };
+        store.commit("SET_USER", currentUser);
+        store.commit("SET_LOGGED_IN", true);
+        store.dispatch("SET_CHAT_USERS");
+      }
+    });
+  },
 });
 </script>
