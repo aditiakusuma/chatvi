653ac94b5902a02ef35472d2b4733811cde64555
aditia kusuma <aditiakusuma800@gmail.com>
2021-01-23 12:37:14 +0700

    add router paths

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,33 @@
-import { createRouter, createWebHistory } from '@ionic/vue-router';
-import { RouteRecordRaw } from 'vue-router';
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from "@ionic/vue-router";
+import { RouteRecordRaw } from "vue-router";
+
+import Home from "../views/Home.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    redirect: '/home'
+    path: "/",
+    redirect: "/home",
   },
   {
-    path: '/home',
-    name: 'Home',
-    component: Home
-  }
-]
+    path: "/home",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/friends",
+    name: "Friends",
+    component: () => import("@/views/FriendList.vue"),
+  },
+  {
+    path: "/chat",
+    name: "Chat",
+    component: () => import("@/views/Chat.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
