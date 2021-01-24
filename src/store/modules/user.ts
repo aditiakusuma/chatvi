5b13485572ef1183d2315bf4d9fba1ff49064fdb
aditia kusuma <aditiakusuma800@gmail.com>
2021-01-24 14:43:33 +0700

    add google plus login plugin

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -1,8 +1,9 @@
 import firebase from "firebase";
 import { User } from "@/models/user";
+import { GooglePlus } from "@ionic-native/google-plus";
 // import { useRouter } from "vue-router";
 
-const GOOGLE_AUTH_PROVIDER = new firebase.auth.GoogleAuthProvider();
+// const GOOGLE_AUTH_PROVIDER = new firebase.auth.GoogleAuthProvider();
 // const router = useRouter();
 
 export default {
@@ -25,35 +26,76 @@ export default {
   },
   actions: {
     SET_USER({ commit }: any) {
-      firebase
-        .auth()
-        .signInWithPopup(GOOGLE_AUTH_PROVIDER)
-        .then((user) => {
-          const currentUser = {
-            uid: user.user?.uid,
-            displayName: user.user?.displayName,
-            email: user.user?.email,
-            photoURL: user.user?.photoURL,
-            phoneNumber: user.user?.phoneNumber,
-          };
+      GooglePlus.login({
+        webClientId:
+          "729986428725-iva1btkjrobck7l2rbjr8344pstdhd5s.apps.googleusercontent.com",
+        offline: true,
+      }).then((res) => {
+        const googleCredential = firebase.auth.GoogleAuthProvider.credential(
+          res.idToken
+        );
+        firebase
+          .auth()
+          .signInWithCredential(googleCredential)
+          .then((user) => {
+            const currentUser = {
+              uid: user.user?.uid,
+              displayName: user.user?.displayName,
+              email: user.user?.email,
+              photoURL: user.user?.photoURL,
+              phoneNumber: user.user?.phoneNumber,
+            };
+            firebase
+              .firestore()
+              .collection("users")
+              .doc(user.user?.uid)
+              .set(currentUser);
 
-          firebase
-            .firestore()
-            .collection("users")
-            .doc(user.user?.uid)
-            .set(currentUser);
-
-          commit("SET_USER", currentUser);
-          commit("SET_LOGGED_IN", true);
-          //   router.push("/friends");
-        })
-        .catch((err) => {
-          commit("SET_FRIENDS", {
-            showing: true,
-            text: err.message,
-            color: "error",
+            commit("SET_USER", currentUser);
+            commit("SET_LOGGED_IN", true);
+            //   router.push("/friends");
+          })
+          .catch((err) => {
+            commit("SET_FRIENDS", {
+              showing: true,
+              text: err.message,
+              color: "error",
+            });
           });
-        });
+      }),
+        (err: any) => {
+          console.log(err);
+        };
+
+      // firebase
+      //   .auth()
+      //   .signInWithPopup(GOOGLE_AUTH_PROVIDER)
+      //   .then((user) => {
+      //     const currentUser = {
+      //       uid: user.user?.uid,
+      //       displayName: user.user?.displayName,
+      //       email: user.user?.email,
+      //       photoURL: user.user?.photoURL,
+      //       phoneNumber: user.user?.phoneNumber,
+      //     };
+
+      //     firebase
+      //       .firestore()
+      //       .collection("users")
+      //       .doc(user.user?.uid)
+      //       .set(currentUser);
+
+      //     commit("SET_USER", currentUser);
+      //     commit("SET_LOGGED_IN", true);
+      //     //   router.push("/friends");
+      //   })
+      //   .catch((err) => {
+      //     commit("SET_FRIENDS", {
+      //       showing: true,
+      //       text: err.message,
+      //       color: "error",
+      //     });
+      //   });
     },
     LOGOUT({ commit }: any) {
       firebase.auth().signOut();
